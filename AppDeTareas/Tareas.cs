5b2d184d9ddc025255e29e6fc7bf6734053d5aac
MatiasAaronSosaCFH <akucfh@gmail.com>
2023-03-11 02:00:32 -0300

    Agregar archivos de proyecto.

---
+++ AppDeTareas/Tareas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDeTareas
+{
+    class Tareas
+    {
+
+        public Tareas(string nombre, int tiempo)
+        {
+
+            this.Nombre = nombre;
+            this.Tiempo = tiempo;
+        }
+
+        private string? Nombre { get; set; }
+        private int Tiempo { get; set; }
+    }
+}
